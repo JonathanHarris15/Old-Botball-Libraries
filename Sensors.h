6236bebc2f4d17ffdb1feb3c86927ac92c785164
Jonathan Harris <46660273+JonathanHarris15@users.noreply.github.com>
2021-02-13 16:47:16 -0600

    Update Sensors.h

--- Sensors.h
+++ Sensors.h
@@ -11,3 +11,44 @@ int get_analog_value(int port, int loops){
   return mass/loops;
 
 }
+
+int colorSense(int color, int loops) {
+
+    camera_open_black();
+    blobCount = 0;
+    blobRaw = 0;
+    
+    while(blobCount < loops){
+
+        camera_update();
+        if(color == 0){
+            if(get_object_confidence(yellow,0) > 0.4){
+                blobRaw = blobRaw + 1;
+            } 
+        }
+
+        if(color == 1){
+            if(get_object_confidence(blue,0) > 0.4){
+                blobRaw = blobRaw + 1; 
+            }
+        }
+
+        if(color == 2){
+            if(get_object_confidence(red,0) > 0.4){
+                blobRaw = blobRaw + 1; 
+            }
+
+        }
+        blobCount = blobCount + 1;
+
+    }
+
+
+
+    if(blobRaw > loops * 65/100){
+        return 0;
+    }else{
+        return 1;
+    }
+
+}
