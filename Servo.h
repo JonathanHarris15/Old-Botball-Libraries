616c207bd8ed08cda2fd053fdcbc0f8e864870ba
Jonathan Harris <46660273+JonathanHarris15@users.noreply.github.com>
2021-02-13 13:32:57 -0600

    Update Servo.h

--- Servo.h
+++ Servo.h
@@ -34,41 +34,61 @@ void servo(int port, int position, int speed){
     
 } 
 
-//servo function by Jacob of Noble High School (written on August 8,2019) EDITED BY: Jonathan Harris(Feb 13, 2021)
-int port;
-int position;
-int speed;
+//this is a modified version of Jacobs servo function for use with threads. dont call this normally and it is only used in the function bellow.
+int thread_servo_port;
+int thread_servo_position;
+int thread_servo_speed;
 void bare_servo(){   //this servo has been modified to be used in threads
 
-    int current = get_servo_position(port);
+    int current = get_servo_position(thread_servo_port);
 
-    if(position > 2047){
+    if(thread_servo_position > 2047){
 
-        position = 2047;
+        thread_servo_position = 2047;
 
     } 
-    if(position < 0){
+    if(thread_servo_position < 0){
 
-        position = 0;
+        thread_servo_position = 0;
 
     }   
 
-    while(current <= position-speed || current >= position+speed){ 
+    while(current <= thread_servo_position-thread_servo_speed || current >= thread_servo_position+thread_servo_speed){ 
 
-        if(current < position){
+        if(current < thread_servo_position){
 
-            current += speed;
+            current += thread_servo_speed;
            
         }
-        if(current > position){
+        if(current > thread_servo_position){
 
-            current -= speed;
+            current -= thread_servo_speed;
 
         }
-        set_servo_position(port, current);
+        set_servo_position(thread_servo_port, current);
         msleep(2);
     }
 
-    set_servo_position(port,position);
+    set_servo_position(thread_servo_port,thread_servo_position);
     
 } 
+//This function was written by Jonathan Harris on Feb 13, 2021
+//this double servo is untested cause I wrote it without using a wallaby or wombat but I cant see anything wrong with it
+//DONT USE THIS COMMAND UNLESS YOU ARE COMFORTABLE WITH THREADS AND HOW THEY WORK, YOU WILL NOT GET THE RESULTS YOU WANT EVERYTIME!!    
+void double_servo_thread(int port1, int pos1, int speed1, int port2, int pos2, int speed2){
+    
+    //defining parameters for the second thread function
+    thread_servo_port = port2;
+    thread_servo_position = pos2;
+    thread_servo_speed = speed2;
+    
+    thread second_servo;
+    second_servo = thread_create(bare_servo);
+    
+    thread_start(second_servo);
+    servo(port1, pos1, speed1);
+    thread_destroy(second_servo);
+    
+    
+    
+}
