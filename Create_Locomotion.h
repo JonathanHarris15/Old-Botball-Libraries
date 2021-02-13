71f6d32e8fb0d2058c575b97c94ef9b1f818fb94
Jonathan Harris <46660273+JonathanHarris15@users.noreply.github.com>
2021-02-13 12:40:04 -0600

    Update Create_Locomotion.h

--- Create_Locomotion.h
+++ Create_Locomotion.h
@@ -66,3 +66,93 @@ void create_turn_left(int deg,int speed)
         printf("the angle of this turn is = %d \n",get_create_normalized_angle());
     }
 }
+
+int bias;
+/*
+This function is used to initiate the gyros for the drive and turn functions
+I would advise using 30 loops but any more than that is really not neccesary
+The calculate bias should be called right BEFORE wait for light and then if needed 
+you can call it again about halfway through but if it is still is inacurate you need to 
+adjust the drive function, not add more loops
+*/
+void calculate_bias(int loops){
+    int i;
+    int bias_mass = 0;
+    mav(right_wheel,0);
+    mav(left_wheel,0);
+    msleep(100);
+    for(i = 0; i < loops; i++){
+        bias_mass = bias_mass + gyro_z();
+        mav(right_wheel,0);
+        mav(left_wheel,0);
+        msleep(5);
+    }
+    bias = bias_mass / loops;
+}
+
+
+/*
+a drive for the create using the gyroscope to maintain a straight line while driving 
+as well as correcting if external sources disrupt the path. The drive is much more exact than a
+reckless drive just using create_drive_direct but it is not 100% accurate. Every sensor has noise in the value
+reading so using square ups and linefollowing at times as well as other solid methods of detection are still advised.
+I WOULD NOT advise using the maximum speed output of the wheels when calling the function. The way the wheels correct 
+is by dropping the speed of one wheel and speeding up the other, so if the wheel are already at max speed it cant correct properly.
+*/
+//DISTANCE IS MEASURED IN CM
+void drive (int speed, int distance){
+    
+    float theta = 0;
+    float error = 0;
+    float target = 0;
+    set_create_distance(0);
+    while(get_create_distance() < distance){
+       
+        create_drive_direct(speed-error*0.1, speed+error*0.1);
+        msleep(15);
+        theta = theta + (gyro_z() - bias);
+        error = (target - (theta));
+        
+        
+    }
+    create_drive_direct(0,0);
+    msleep(20);
+    
+}
+
+/*
+turns for the create using the gyroscope. THE TARGET IS NOT IN DEGREES and you will 
+need to take the time to figure out what values get you the turns you need. Another thing to note
+is that the bot WILL turn until it reaches its target so if something impairs its path it will get stuck on the turn.
+Although the turn is fairly accurate it is not 100% so you will need to check your rotation every once in a while with a square up.
+*/
+
+void right(int speed, int target){
+    
+    float theta = 0;
+    while(theta > -target){
+      
+        create_drive_direct(speed, -speed);
+        msleep(10); 
+        theta = theta + ((gyro_z() - bias) * 0.03);
+        
+    }
+    create_drive_direct(0,0);
+    msleep(20);
+    
+}
+
+void left(int speed, int target){
+    
+    float theta = 0; 
+    while(theta < target){
+        
+        create_drive_direct(-speed, speed);
+        msleep(10);
+        theta = theta + ((gyro_z() - bias)  * 0.03);
+        
+    }
+    create_drive_direct(0,0);
+    msleep(20);
+    
+}
